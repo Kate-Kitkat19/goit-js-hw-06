ebd9923019f072913c46279e19eae6179b6cd0fd
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-03 21:05:10 +0300

    first task done

--- js/task-01.js
+++ js/task-01.js
@@ -4,20 +4,9 @@ const message = `Number of categories: ${totalNumber}`;
 
 console.log(message);
 
-let numberOfSubitems = [];
-numberOfSubitems = [...mainItems].forEach((item) => item.querySelector("h2"));
-console.log(numberOfSubitems);
-
-// _______________________________________________
-
-// const categoryNames = document.querySelectorAll("h2");
-// console.log(categoryNames);
-// const lists = [...categoryNames].map((category) => category.children);
-// [...lists].map((list) => console.log(list));
-
-// const itemsAll = document.querySelectorAll("li").length;
-// console.log(itemsAll);
-
-// const names = [];
-// categoryNames.forEach((name) => names.push(name.textContent));
-// console.log(names);
+mainItems.forEach((item) => {
+  const nameOfCategory = item.querySelector("h2");
+  console.log(`Category: ${nameOfCategory.textContent}`);
+  const numberOfSubitems = item.querySelectorAll("li").length;
+  console.log(`Elements: ${numberOfSubitems}`);
+});

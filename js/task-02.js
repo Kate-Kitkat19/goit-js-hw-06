f3e06dd9a345a71e5edea2070a62b25fc04a2d24
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-06 19:38:36 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = [];
+
+ingredients.forEach((item) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = item;
+  listItem.classList.add("item");
+  ingredientsList.push(listItem);
+});
+
+const listRef = document.querySelector("#ingredients");
+listRef.append(...ingredientsList);

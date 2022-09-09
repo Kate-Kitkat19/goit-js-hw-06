8fe7a43568bf35c6c300e0c2c2deab602c6ab631
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-09 17:25:23 +0300

    task 6 done

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputRef = document.querySelector("#validation-input");
+const lengthRef = Number(inputRef.dataset.length);
+
+function onInputBlur(event) {
+  if (event.currentTarget.value.length !== lengthRef) {
+    inputRef.classList.remove("valid");
+    inputRef.classList.add("invalid");
+  } else {
+    inputRef.classList.remove("invalid");
+    inputRef.classList.add("valid");
+  }
+}
+
+inputRef.addEventListener("blur", onInputBlur);

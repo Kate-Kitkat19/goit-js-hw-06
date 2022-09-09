83be3f0d061a81b38a68fdf60b81ee831853ffb9
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-09 22:29:12 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputRef = document.querySelector('input[type="number"]');
+const outputRef = document.querySelector("div#boxes");
+
+function createBoxes(amount) {
+  amount = event.currentTarget.value;
+  const numbersArray = [];
+  for (let i = 0; i < amount; i++) {
+    numbersArray.push(i);
+    console.log(numbersArray);
+  }
+  const markup = numbersArray.forEach(() => document.createElement("div"));
+  console.log(markup);
+}
+
+inputRef.addEventListener("change", createBoxes);

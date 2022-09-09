9fb0f241a1d05e5483800bfbfd728deaa6faf5da
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-09 20:44:54 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields");
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
+
+formRef.addEventListener("submit", onFormSubmit);

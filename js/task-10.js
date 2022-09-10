064978d9afe549640b5e4e27ce909df27809663d
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-10 12:12:03 +0300

    task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -4,16 +4,33 @@ function getRandomHexColor() {
 
 const inputRef = document.querySelector('input[type="number"]');
 const outputRef = document.querySelector("div#boxes");
+const divsArray = [];
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
 
 function createBoxes(amount) {
   amount = event.currentTarget.value;
   const numbersArray = [];
   for (let i = 0; i < amount; i++) {
     numbersArray.push(i);
-    console.log(numbersArray);
   }
-  const markup = numbersArray.forEach(() => document.createElement("div"));
-  console.log(markup);
+  let width = 30;
+  numbersArray.forEach(() => {
+    const element = document.createElement("div");
+    element.style.backgroundColor = getRandomHexColor();
+    width += 10;
+    element.style.width = `${width}px`;
+    element.style.height = `${width}px`;
+    divsArray.push(element);
+  });
 }
 
 inputRef.addEventListener("change", createBoxes);
+createBtn.addEventListener("click", () => {
+  outputRef.append(...divsArray);
+});
+destroyBtn.addEventListener("click", () => {
+  outputRef.innerHTML = "";
+  inputRef.value = "";
+  divsArray.length = 0;
+});
